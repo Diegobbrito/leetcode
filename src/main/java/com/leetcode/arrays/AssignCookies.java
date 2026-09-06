8bb6e0906a8fb1f965cbdec0b25d80519e22aa8a
Diego Brito <dibbrito@gmail.com>
2026-09-06 13:37:54 -0300

    refactor: simplify variable usage in AssignCookies class

--- src/main/java/com/leetcode/arrays/AssignCookies.java
+++ src/main/java/com/leetcode/arrays/AssignCookies.java
@@ -4,16 +4,14 @@ import java.util.Arrays;
 
 public class AssignCookies {
     //    https://leetcode.com/problems/assign-cookies/
-//    Time Complexity O(GlogG + SlogS + Max g|s)
-//    Memory Complexity O(g + s)
+    //    Time Complexity O(GlogG + SlogS + Max g|s)
+    //    Memory Complexity O(g + s)
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
         int j = 0;
         int i = 0;
-        int gSize = g.length;
-        int sSize = s.length;
-        while(i < gSize && j < sSize){
+        while(i < g.length && j < s.length){
             if(g[i] <= s[j]){
                 i++;
             } 
