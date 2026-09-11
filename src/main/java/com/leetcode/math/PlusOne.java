c3a9314afcbf909da1440bc70af346088b97c958
Diego Brito <dibbrito@gmail.com>
2026-09-11 19:13:36 -0300

    refactor: optimize plusOne method for clarity and efficiency

--- src/main/java/com/leetcode/math/PlusOne.java
+++ src/main/java/com/leetcode/math/PlusOne.java
@@ -4,23 +4,18 @@ public class PlusOne {
 //    https://leetcode.com/problems/plus-one/
 
     //    Time complexity: O(n), where n is the number of digits in the input array.
-//    Space complexity: O(1)
+    //    Space complexity: O(1)
     public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) { // if the digit is less than 9, we can increment it and return the array
-                digits[i]++;
-                break;
+
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i] = digits[i] + 1;
+                return digits;
             }
-            digits[i] = 0; // if the digit is 9, we set it to 0 and continue to the next digit
-            if (i == 0) { // if we reach the first digit, we need to add a new digit to the array
-                int[] result = new int[digits.length + 1];
-                result[0] = 1;
-                for (int j = 0; j < digits.length; j++) {
-                    result[j + 1] = digits[j];
-                }
-                return result;
-            }
+            digits[i] = 0;
         }
-        return digits;
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
     }
 }
