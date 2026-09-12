d936c9dd015e05d05e5d4b201df7296020308346
Diego Brito <dibbrito@gmail.com>
2026-09-12 13:11:01 -0300

    feat: add RangeSumQueryImmutable class with methods for range sum queries

---
+++ src/main/java/com/leetcode/prefix/RangeSumQueryImmutable.java
@@ -0,0 +1,26 @@
+package com.leetcode.prefix;
+
+public class RangeSumQueryImmutable {
+
+//    https://neetcode.io/problems/range-sum-query-immutable/
+//    Time complexity: O(n) for constructor, O(1) for sumRange
+//    Space complexity: O(n)
+
+    int[] prefix;
+    public RangeSumQueryImmutable(int[] nums) {
+        this.prefix = new int[nums.length];
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            prefix[i] = sum;
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        if(left == 0){
+            return prefix[right];
+        }
+
+        return prefix[right] - prefix[left - 1];
+    }
+}
